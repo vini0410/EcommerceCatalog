d70395c2397696096fd510e64306fb1645ff9b83
vini0410 <vini0420@gmail.com>
2025-07-19 19:50:03 -0300

    add port e remove ws config

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,9 @@
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
 import * as schema from "@shared/schema";
+import dotenv from 'dotenv';
 
-neonConfig.webSocketConstructor = ws;
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
